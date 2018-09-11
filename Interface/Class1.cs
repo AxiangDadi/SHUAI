895e71a6837f56d7664cacd5977dabc438e5c8c6
AxiangDadi <421681437@qq.com>
2018-09-11 17:36:23 +0800

    qwertyuio

--- Interface/Class1.cs
+++ Interface/Class1.cs
@@ -5,13 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Interface
-{
+{ 
     class Class1
     {
         public void ShowYourMoth() {
             Console.WriteLine("我还要测试一遍");
-            Console.WriteLine("刘坤是煞笔");
-            Console.WriteLine("刘坤是煞笔");
         }
     }
 }
