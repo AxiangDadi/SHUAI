a4537de8f013663c9fc6bb28c71236003c33208a
AxiangDadi <421681437@qq.com>
2018-09-11 17:22:36 +0800

    ss

--- Interface/Class1.cs
+++ Interface/Class1.cs
@@ -12,7 +12,7 @@ namespace Interface
             Console.WriteLine("我还要测试一遍");
             Console.WriteLine("刘坤是煞笔");
             Console.WriteLine("刘坤是煞笔");
-            Console.WriteLine("qwerty");
+            Console.WriteLine("wq2edqwdqwdqww");
         }
     }
 }
