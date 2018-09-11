761cb0d203469bb59b50a008a51bb01bcd6938a3
AxiangDadi <421681437@qq.com>
2018-09-11 17:00:48 +0800

    shuchu

--- Interface/Class1.cs
+++ Interface/Class1.cs
@@ -12,6 +12,7 @@ namespace Interface
             Console.WriteLine("我还要测试一遍");
             Console.WriteLine("刘坤是煞笔");
             Console.WriteLine("刘坤是煞笔");
+            Console.WriteLine("qwerty");
         }
     }
 }
