4b1b6e3f9ab056496ab8e20d78f59988f97122ec
AxiangDadi <421681437@qq.com>
2018-09-11 17:40:21 +0800

    zx

--- Interface/Class1.cs
+++ Interface/Class1.cs
@@ -10,8 +10,6 @@ namespace Interface
     {
         public void ShowYourMoth() {
             Console.WriteLine("我还要测试一遍");
-            Console.WriteLine("刘坤是煞笔");
-            Console.WriteLine("刘坤是煞笔");
         }
     }
 }
